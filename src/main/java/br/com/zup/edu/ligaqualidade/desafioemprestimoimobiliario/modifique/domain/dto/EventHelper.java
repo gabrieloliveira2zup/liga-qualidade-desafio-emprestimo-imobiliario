5eb95c48f373faebbd5d107b7322cfb4abc315a7
gabrieloliveira2zup <73697815+gabrieloliveira2zup@users.noreply.github.com>
2021-03-06 13:33:06 -0300

    Create EventHelper.java

---
+++ src/main/java/br/com/zup/edu/ligaqualidade/desafioemprestimoimobiliario/modifique/domain/dto/EventHelper.java
@@ -0,0 +1,12 @@
+package br.com.zup.edu.ligaqualidade.desafioemprestimoimobiliario.modifique.domain.dto;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class EventHelper {
+
+  public final Set<Proposal> proposalSet = new HashSet<>();
+  public final Set<Proponent> proponentSet = new HashSet<>();
+  public final Set<Warranty> warrantySet = new HashSet<>();
+
+}
