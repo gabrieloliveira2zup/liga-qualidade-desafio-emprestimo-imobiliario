863add168c38ed953ff9fae2aa66420432d6025b
gabrieloliveira2zup <73697815+gabrieloliveira2zup@users.noreply.github.com>
2021-03-06 13:21:46 -0300

    Update Solucao.java

--- src/main/java/br/com/zup/edu/ligaqualidade/desafioemprestimoimobiliario/modifique/Solucao.java
+++ src/main/java/br/com/zup/edu/ligaqualidade/desafioemprestimoimobiliario/modifique/Solucao.java
@@ -1,10 +1,16 @@
 package br.com.zup.edu.ligaqualidade.desafioemprestimoimobiliario.modifique;
 
+import br.com.zup.edu.ligaqualidade.desafioemprestimoimobiliario.modifique.domain.dto.EventHelper;
+import br.com.zup.edu.ligaqualidade.desafioemprestimoimobiliario.modifique.domain.service.parser.handler.impl.ParserHandlerService;
 import java.util.List;
+import java.util.Map;
 
 public class Solucao {
 	
   public static String processMessages(List<String> messages) {
+    ParserHandlerService parserHandlerService = new ParserHandlerService();
+    EventHelper eventHelper = parserHandlerService.parseMessages(messages);
+
 	  return ""; 
   }
 }
